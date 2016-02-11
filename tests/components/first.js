7be48bb3ca7d8d1142bdbe4042a27cdaa99b3a77
Samar Panda <er.samarpanda@gmail.com>
2016-02-11 07:43:24 +0530

    Mocha test setup

---
+++ tests/components/first.js
@@ -0,0 +1,10 @@
+var ReactTestUtils = require('react-addons-test-utils');
+var assert = require('assert');
+
+//var First = require('../../app/components/first');
+
+describe('First Component', function(){
+	it('First Test', function(){
+		assert(true === true);
+	})
+});
